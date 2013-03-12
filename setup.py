5c50b08214c81e0501cf9384a466c564703a2584
Daniel Yavorovich <daniel@quietsupport.net>
2013-03-12 16:33:32 +0200

    add function for backup directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='s3mysqlbkp',
-    version='1.2',
+    version='1.3',
     description='MySQL backups to Amazon S3',
     author='Daniel Yavorovich',
     author_email='daniel@quietsupport.net',
@@ -13,7 +13,7 @@ setup(name='s3mysqlbkp',
     packages=['s3mysqlbkp'],
     package_dir={'s3mysqlbkp': 'src/s3mysqlbkp'},
     scripts = ['bin/s3mysqlbkp_run.py'],
-    download_url = 'http://pip.hosting4django.net/s3mysqlbkp-1.2.tar.gz',
+    download_url = 'http://pip.hosting4django.net/s3mysqlbkp-1.3.tar.gz',
     data_files=[
         ('../etc', ['cfg/s3mysqlbkp.conf']),
     ],
