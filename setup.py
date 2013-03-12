916890b49ef56ccbd99336d099724fb9914ac578
Daniel Yavorovich <daniel@quietsupport.net>
2013-03-12 15:48:59 +0200

    change package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from distutils.core import setup
 
 setup(name='s3mysqlbkp',
-    version='1.1',
+    version='1.2',
     description='MySQL backups to Amazon S3',
     author='Daniel Yavorovich',
     author_email='yavorovich@denni.org',
@@ -13,7 +13,7 @@ setup(name='s3mysqlbkp',
     packages=['s3mysqlbkp'],
     package_dir={'s3mysqlbkp': 'src/s3mysqlbkp'},
     scripts = ['bin/s3mysqlbkp_run.py'],
-    download_url = 'http://pip.hosting4django.net/s3mysqlbkp-1.1.tar.gz',
+    download_url = 'http://pip.hosting4django.net/s3mysqlbkp-1.2.tar.gz',
     data_files=[
         ('../etc', ['cfg/s3mysqlbkp.conf']),
     ],
